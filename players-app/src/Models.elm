0f2c071ffb2f3cfc9d06d111cb73cfc3944d0ca2
kenshero <teeruk@proteus-tech.com>
2017-04-02 04:58:36 +0700

    first commit

---
+++ players-app/src/Models.elm
@@ -0,0 +1,30 @@
+module Models exposing (..)
+
+import RemoteData exposing (WebData)
+
+
+type alias Model =
+    { players : WebData (List Player)
+    , route : Route
+    }
+
+type Route
+    = PlayersRoute
+    | PlayerRoute PlayerId
+    | NotFoundRoute
+
+initialModel : Route -> Model
+initialModel route =
+    { players = RemoteData.Loading
+    , route = route
+    }
+
+
+type alias PlayerId =
+    String
+
+type alias Player =
+    { id : PlayerId
+    , name : String
+    , level : Int
+    }
